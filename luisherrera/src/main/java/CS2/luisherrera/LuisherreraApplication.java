61e3b030a5c4716edddf332bbea9ff29322902cd
Alejandro Herrera <alejogamehg@gmail.com>
2025-08-16 11:37:32 -0500

    initial commit

---
+++ luisherrera/src/main/java/CS2/luisherrera/LuisherreraApplication.java
@@ -0,0 +1,13 @@
+package CS2.luisherrera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuisherreraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuisherreraApplication.class, args);
+	}
+
+}
